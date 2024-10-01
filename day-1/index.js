1db3293c403b2f7d1b3dbb9d09e9bb40becdbc90
yansiii <isotiyayansi@gmail.com>
2024-10-01 13:49:20 +0530

    first commit

---
+++ day-1/index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const port = 8000;
+
+const portHandler = (req,res)=>{
+    res.write("<h1> server started </h1>")
+    res.end()
+}
+
+let server = http.createServer(portHandler)
+
+server.listen(port,(err)=>{
+    err ? console.log(err): console.log("server started on port " + port);
+})
